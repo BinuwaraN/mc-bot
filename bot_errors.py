0e0612ecd982e28885d298526b681131e73720b3
BinuwaraN <41659980+BinuwaraN@users.noreply.github.com>
2021-05-30 15:42:13 +1000

    Remove unecssary exceptions

--- bot_errors.py
+++ bot_errors.py
@@ -2,19 +2,3 @@ class InvalidRefreshRate(Exception):
     def __init__(self, refresh_rate):
         super().__init__(
             f"Refresh rate must be 30 or higher. You have it set to {refresh_rate}.")
-
-
-class NotOnServerError(Exception):
-    """You need to run this command on 24CC"""
-
-
-class NoPermissionError(Exception):
-    pass
-
-
-class FuckyWucky:
-    """You made one."""
-
-
-class EmptryString(Exception):
-    """Empty string provided"""
